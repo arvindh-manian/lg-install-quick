a7584d00a03c8517bc801b7c4c7eea8cbeb47f65
Arvindh Manian <arvindh.manian@gmail.com>
2019-12-30 16:48:51 -0500

    Adding some prereqs for liquid galaxy install

--- remove-bloat.sh
+++ remove-bloat.sh
@@ -1,1 +1,4 @@
 sudo apt-get remove account-plugin* aisleriot brltty duplicity empathy empathy-common example-content gnome-accessibility-themes gnome-contacts gnome-mahjongg gnome-mines gnome-orca gnome-screensaver gnome-sudoku gnome-video-effects gnomine landscape-common libreoffice* libsane* mcp-account-manager-uoa python3-uno rhythmbox* shotwell shotwell-common telepathy* totem* printer-driver* thunderbird*
+sudo apt install lsb
+sudo apt install lsb-core
+sudo apt install lightdm
