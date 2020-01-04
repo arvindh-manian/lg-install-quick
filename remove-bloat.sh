45cac71e0e9a5120def88a29301ccbe1e8a7b503
Arvindh Manian <arvindh.manian@gmail.com>
2020-01-04 17:27:34 -0500

    Add LG installation

--- remove-bloat.sh
+++ remove-bloat.sh
@@ -1,2 +1,3 @@
 sudo apt-get remove account-plugin* aisleriot brltty duplicity empathy empathy-common example-content gnome-accessibility-themes gnome-contacts gnome-mahjongg gnome-mines gnome-orca gnome-screensaver gnome-sudoku gnome-video-effects gnomine landscape-common libreoffice* libsane* mcp-account-manager-uoa python3-uno rhythmbox* shotwell shotwell-common telepathy* totem* printer-driver* thunderbird*
 sudo apt install -y lsb lsb-core lightdm
+bash <(curl -s https://raw.githubusercontent.com/LiquidGalaxyLAB/liquid-galaxy/master/install.sh)
