3e28d2c29430c409bb448b41489f151ad4ae021d
Arvindh Manian <arvindh.manian@gmail.com>
2020-01-04 13:46:31 -0500

    Update remove-bloat.sh

--- remove-bloat.sh
+++ remove-bloat.sh
@@ -1,4 +1,2 @@
 sudo apt-get remove account-plugin* aisleriot brltty duplicity empathy empathy-common example-content gnome-accessibility-themes gnome-contacts gnome-mahjongg gnome-mines gnome-orca gnome-screensaver gnome-sudoku gnome-video-effects gnomine landscape-common libreoffice* libsane* mcp-account-manager-uoa python3-uno rhythmbox* shotwell shotwell-common telepathy* totem* printer-driver* thunderbird*
-sudo apt install lsb
-sudo apt install lsb-core
-sudo apt install lightdm
+sudo apt install -y lsb lsb-core lightdm
